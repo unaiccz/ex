58952a45e214c8d1e0c3a6d6b4c4c87f8ac2b5ef
unaiccz <unaicc2003@gmail.com>
2023-01-09 12:27:17 +0100

    yyy

--- eliminar.php
+++ eliminar.php
@@ -1,10 +1,10 @@
 <?php
 
-$id = $GET['id'];
+$id = $_GET['id'];
+
 $conn = mysqli_connect('localhost', 'root', '', 'examenes');
 $query = "DELETE FROM examen where id = '$id'";
-$cons = $constt =mysqli_query($conn, $query);
-echo $id;
+ $constt=mysqli_query($conn, $query);
 
 
 
