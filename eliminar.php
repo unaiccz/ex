151429c1b1f311bacdfd11806796155b4c4e2f3d
Cosmin <75861657+cosmind-rusu@users.noreply.github.com>
2023-01-09 12:38:56 +0100

    Control de errores con mysql

--- eliminar.php
+++ eliminar.php
@@ -1,11 +1,24 @@
 <?php
 
-$id = $_GET['id'];
-
+// Conexión a la base de datos
 $conn = mysqli_connect('localhost', 'root', '', 'examenes');
-$query = "DELETE FROM examen where id = '$id'";
- $constt=mysqli_query($conn, $query);
 
+// Si hay algún error de conexión, mostramos un mensaje y terminamos la ejecución del script
+if (mysqli_connect_error()) {
+    die('Error de conexión a la base de datos: ' . mysqli_connect_error());
+}
 
+// Obtenemos el valor de la variable 'id' de la cadena de consulta de la URL
+$id = isset($_GET['id']) ? mysqli_real_escape_string($conn, $_GET['id']) : '';
 
-?>
+// Preparamos la consulta SQL utilizando una consulta preparada y parámetros para evitar la inyección de SQL
+$stmt = mysqli_prepare($conn, "DELETE FROM examen WHERE id = ?");
+mysqli_stmt_bind_param($stmt, "s", $id);
+
+// Ejecutamos la consulta
+if (!mysqli_stmt_execute($stmt)) {
+    die('Error al ejecutar la consulta: ' . mysqli_error($conn));
+}
+
+// Cerramos la conexión a la base de datos
+mysqli_close($conn);
